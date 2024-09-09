630b1bb1d6c8382e2c0e5e929017048c19130618
Quan Dang <18520339@gm.uit.edu.vn>
2024-09-10 02:40:24 +1000

    Pruning for each layer and implement adaptive terms for SimulatedAnnealingPruner

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -10,20 +10,14 @@ pd.set_option('display.max_columns', None)
 class SimulatedAnnealingPruner(Pruner):
     def __init__(
         self, baseline, initial_temperature=1.0, iterations=200,
-        mutation_rate=0.05, max_loss=1.0, max_loss_penalty=1e8
+        mutation_rate=0.05, target_loss=1.0, max_loss_penalty=1e8
     ):
-        super().__init__(baseline, max_loss, max_loss_penalty)
+        super().__init__(baseline, target_loss, max_loss_penalty)
         self.initial_temperature = initial_temperature
         self.iterations = iterations
         self.mutation_rate = mutation_rate
-
-    def _generate_neighbor(self):
-        layer_index = np.random.choice(
-            self.baseline.prunable_layers,
-            p=[self.baseline.layer_importances[i] for i in self.baseline.prunable_layers]
-        )
-        layer_mask = self.masks[layer_index] * ~(np.random.rand(*self.masks[layer_index].shape) < self.mutation_rate)
-        return layer_index, layer_mask
+        self.target_loss = target_loss
+        self.display_handle = display('', display_id=True)
 
     def _acceptance_probability(self, deltaE, temperature):
         prob = np.exp(-deltaE / temperature)
@@ -31,32 +25,38 @@ class SimulatedAnnealingPruner(Pruner):
         return np.random.uniform(0, 1) < prob
 
     def prune(self):
-        current_obj_dict = self.calculate_objective()
-        best_obj_dict = current_obj_dict
-        display_handle = display("", display_id=True)
+        for layer_index, layer in enumerate(self.baseline.model.layers):
+            if len(layer.get_weights()) <= 0 : continue
+            self.max_loss = self.target_loss * (layer_index + 1) / len(self.baseline.model.layers) # Adaptive max_loss
+            current_obj_dict = self.calculate_objective()
+            best_obj_dict = current_obj_dict
 
-        for step in tqdm(range(self.iterations)):
-            temperature = self.initial_temperature / (1 + np.log(1 + step))
-            if temperature <= 0: break
+            for step in tqdm(range(self.iterations), desc=f'[LAYER {layer_index}]'):
+                adaptive_mutation_rate = self.mutation_rate * (1 - step / self.iterations)
+                temperature = self.initial_temperature / (1 + np.log(1 + step))
+                if temperature <= 0: break
 
-            layer_index, layer_mask = self._generate_neighbor()
-            old_layer_mask = self.get_layer_mask(layer_index)
-            self.apply_layer_mask(layer_index, layer_mask)
+                layer_index, layer_mask = self.masks[layer_index] * ~(np.random.rand(*self.masks[layer_index].shape) < adaptive_mutation_rate)
+                old_layer_mask = self.get_layer_mask(layer_index)
+                self.apply_layer_mask(layer_index, layer_mask)
 
-            new_obj_dict = self.calculate_objective()
-            deltaE = new_obj_dict['cost'] - current_obj_dict['cost']
+                new_obj_dict = self.calculate_objective()
+                deltaE = new_obj_dict['cost'] - current_obj_dict['cost']
 
-            if deltaE < 0 or self._acceptance_probability(deltaE, temperature):
-                current_obj_dict = new_obj_dict
-                self.history.append({'layer': layer_index, 'temperature': temperature, **current_obj_dict})
-                display_handle.update(pd.DataFrame(self.history))
+                if deltaE < 0 or self._acceptance_probability(deltaE, temperature):
+                    current_obj_dict = new_obj_dict
+                    self.history.append({
+                        'layer': layer_index, 'temperature': temperature, 
+                        'mutation_rate': adaptive_mutation_rate, **current_obj_dict
+                    })
+                    self.display_handle.update(pd.DataFrame(self.history))
 
-                if current_obj_dict['cost'] < best_obj_dict['cost']:
-                    best_masks = [mask.copy() if mask is not None else None for mask in self.masks]
-                    best_obj_dict = current_obj_dict
-            else:
-                self.reset_layer_weights(layer_index)
-                self.apply_layer_mask(layer_index, old_layer_mask)
+                    if current_obj_dict['cost'] < best_obj_dict['cost']:
+                        best_masks = [mask.copy() if mask is not None else None for mask in self.masks]
+                        best_obj_dict = current_obj_dict
+                else:
+                    self.reset_layer_weights(layer_index)
+                    self.apply_layer_mask(layer_index, old_layer_mask)
 
         self.masks = best_masks
         self.apply_all_masks()
