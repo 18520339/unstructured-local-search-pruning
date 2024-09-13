9c7f6f80352e8f18cabe3732f2f7b111eede0681
Quan Dang <18520339@gm.uit.edu.vn>
2024-09-13 16:11:29 +1000

    Add comments for base.py

--- base.py
+++ base.py
@@ -24,7 +24,7 @@ class MNIST:
         fig, axes = plt.subplots(1, num_images)
         for i in range(num_images):
             axes[i].imshow(self.x_train[i], cmap='gray')
-            axes[i].set_title(f"Label: {np.argmax(self.y_train[i])}")
+            axes[i].set_title(f"Label: {self.y_train[i]}")
             axes[i].axis('off')
         plt.tight_layout()
         plt.show()
@@ -62,7 +62,10 @@ class Baseline:
         return self.model.summary(show_trainable=True)
 
     def train(self, val_ratio=0.2, epochs=50, batch_size=128):
+        # Split training data into training and validation sets
         x_train, x_val, y_train, y_val = train_test_split(self.data.x_train, self.data.y_train, test_size=val_ratio)
+
+        # Train model and save it, then obtain necessary information for pruning process
         self.history = self.model.fit(
             x_train, y_train,
             validation_data=(x_val, y_val),
@@ -74,14 +77,15 @@ class Baseline:
         self.calculate_pruning_info()
 
     def calculate_pruning_info(self):
+        # Calculate the loss on test set, metrics like accuracy, prunable layers, and total weights
         self.loss, self.metrics = self.model.evaluate(self.data.x_test, self.data.y_test, verbose=0)
         self.prunable_layers, self.total_weights = [], 0
 
         for layer_index, layer in enumerate(self.model.layers):
             params = layer.get_weights()
-            if len(params) > 0:
+            if len(params) > 0: # Skip layers with no weights (e.g., activation layers)
                 self.prunable_layers.append(layer_index)
-                self.total_weights += params[0].size
+                self.total_weights += params[0].size # Add number of weights in the layer
 
     def plot_training_history(self):
         plt.figure(figsize=(12, 4))
@@ -89,17 +93,17 @@ class Baseline:
         plt.subplot(1, 2, 1)
         plt.plot(self.history['accuracy'], label='Training Accuracy')
         plt.plot(self.history['val_accuracy'], label='Validation Accuracy')
-        plt.title('Model Accuracy')
         plt.xlabel('Epoch')
         plt.ylabel('Accuracy')
+        plt.title('Model Accuracy')
         plt.legend()
 
         plt.subplot(1, 2, 2)
         plt.plot(self.history['loss'], label='Training Loss')
         plt.plot(self.history['val_loss'], label='Validation Loss')
-        plt.title('Model Loss')
         plt.xlabel('Epoch')
         plt.ylabel('Loss')
+        plt.title('Model Loss')
         plt.legend()
 
         plt.tight_layout()
@@ -110,9 +114,9 @@ class Pruner(ABC):
     def __init__(self, baseline, max_loss=1.0, max_loss_penalty=1e8):
         self.baseline = baseline
         self.pruned_model = load_model(baseline.model_path)
-        self.masks = self._initialize_masks()
-        self.max_loss = max_loss
-        self.max_loss_penalty = max_loss_penalty
+        self.masks = self._initialize_masks() # Initialize masks for all layers
+        self.max_loss = max_loss # Maximum allowed loss
+        self.max_loss_penalty = max_loss_penalty # Penalty for exceeding that maximum loss
         self.history = []
 
     @abstractmethod
@@ -122,7 +126,7 @@ class Pruner(ABC):
     def _initialize_masks(self):
         masks = [] # Initialize masks as all ones (no pruning)
         for layer in self.baseline.model.layers:
-            if len(layer.get_weights()) > 0:  # Skip layers with no weights (e.g., activation layers)
+            if len(layer.get_weights()) > 0: # Skip layers with no weights (e.g., activation layers)
                 weight_mask = np.ones_like(layer.get_weights()[0]) # Weight-level pruning mask
                 masks.append(weight_mask)
             else: masks.append(None)
@@ -131,9 +135,9 @@ class Pruner(ABC):
     def apply_all_masks(self):
         self.pruned_model = load_model(self.baseline.model_path)
         for layer, mask in zip(self.pruned_model.layers, self.masks):
-            if mask is not None:
+            if mask is not None:  # Skip layers with no weights (e.g., activation layers)
                 weights, biases = layer.get_weights()
-                weights *= mask
+                weights *= mask # Apply mask to weights
                 layer.set_weights([weights, biases])
 
     def get_layer_mask(self, layer_index):
@@ -142,10 +146,10 @@ class Pruner(ABC):
 
     def apply_layer_mask(self, layer_index, layer_mask):
         if 0 <= layer_index < len(self.pruned_model.layers) and self.masks[layer_index] is not None:
-            self.masks[layer_index] = layer_mask
+            self.masks[layer_index] = layer_mask # Update mask for the layer
             layer = self.pruned_model.layers[layer_index]
             weights, biases = layer.get_weights()
-            weights *= layer_mask
+            weights *= layer_mask # Apply mask to weights of the layer
             layer.set_weights([weights, biases])
         else: raise ValueError('Invalid layer index')
 
@@ -156,13 +160,18 @@ class Pruner(ABC):
 
     def reset_layer_weights(self, layer_index):
         if 0 <= layer_index < len(self.pruned_model.layers):
-            weights, biases = self.baseline.model.layers[layer_index].get_weights()
-            self.pruned_model.layers[layer_index].set_weights([weights, biases])
+            weights, biases = self.baseline.model.layers[layer_index].get_weights() # Get original weights
+            self.pruned_model.layers[layer_index].set_weights([weights, biases]) # Reset weights
         else: raise ValueError('Invalid layer index')
 
     def calculate_objective(self):
+        # Calculate cost as a combination of relative loss, sparsity, and a penalty for exceeding the maximum allowed loss
         loss, metrics = self.pruned_model.evaluate(self.baseline.data.x_test, self.baseline.data.y_test, verbose=0)
-        sparsity = np.mean([np.mean(mask == 0) for mask in self.masks if mask is not None])
+        sparsity = np.mean([np.mean(mask == 0) for mask in self.masks if mask is not None]) # The ratio of 0 masks in all layers
+
+        # The loss difference is squared to penalize high loss more than low loss
         loss_diff = (self.baseline.loss - loss) ** 2 if loss < self.max_loss else self.max_loss_penalty
-        cost = loss_diff + 1 / (sparsity + 1e-8)
+
+        # Balance between maintaining loss and achieving target sparsity
+        cost = loss_diff + 1 / (sparsity + 1e-8) # # A small epsilon value is added to avoid division by 0
         return {'cost': cost, 'metrics': metrics, 'loss': loss, 'sparsity': sparsity}
